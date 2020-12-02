478d851b5da95d3dcbcfdcfdd216575115c059b8
Hristian Vitrychenko <hristianvitrychenko@gmail.com>
2020-12-02 16:37:08 +0400

    Finished Foundation Task 5

--- FoundationTask2.php
+++ FoundationTask2.php
@@ -1,11 +1,11 @@
 <?php
-    function fibonacciLoop()
+    function fibonacciLoop($max)
     {
         $curr = 0;
         $next = 1;
 
         $counter = 0;
-        while ($counter <= 34){
+        while ($counter <= $max){
             echo $curr."</br>";
             $temp = $next;
             $next = $curr + $next;
@@ -25,11 +25,17 @@
         }
     }
 
-    echo "Loop:"."</br>";
-    fibonacciLoop();
+    /*echo "Loop:"."</br>";
+    fibonacciLoop(63);
     echo "</br>";
     echo "Recursive:"."</br>";
     for($i = 0; $i < 10; $i++)
     {
         echo fibonacciRecur($i)."</br>";
+    }*/
+
+    if(isset($_POST['fibonum']))
+    {
+        $fibonum = $_POST['fibonum'];
+        fibonacciLoop($fibonum);
     }
